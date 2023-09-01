6a878439983513db5742b0affa68e6763d2459ab
elomedeeps <143777572+elomedeeps@users.noreply.github.com>
2023-09-01 17:13:35 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://thevercel.com' + encodeURI(path as string)
+					'https://thevercel.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
